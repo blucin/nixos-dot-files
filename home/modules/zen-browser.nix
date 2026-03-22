b2b9716edb0b66611f7905e37fb4f595b7fc0a64
blucin <blucin@users.noreply.github.com>
2026-03-22 17:55:40 +0530

    Remove xdg migration supress from zen browser (no longer needed)

--- home/modules/zen-browser.nix
+++ home/modules/zen-browser.nix
@@ -5,7 +5,6 @@
   ];
   programs.zen-browser = {
     enable = true;
-    suppressXdgMigrationWarning = true;
     profiles = {
       default = {
         # bookmarks, extensions, search engines ...
